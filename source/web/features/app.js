c3ef01e3faee9c92d7c510222d2530decb354eb3
Einar Ingebrigtsen <einar@dolittle.com>
2018-06-19 15:00:33 +0200

    Started moving across from the legacy design

--- source/web/features/app.js
+++ source/web/features/app.js
@@ -1,6 +1,4 @@
 import { PLATFORM } from 'aurelia-pal';
-import style from '../styles/style.scss';
-import { inject } from 'aurelia-dependency-injection';
 
 export class app {
     constructor() {
@@ -9,7 +7,7 @@ export class app {
     configureRouter(config, router) {
         config.options.pushState = true;
         config.map([
-            { route: ['', 'index'], name: 'index', moduleId: PLATFORM.moduleName('index'), layoutView: PLATFORM.moduleName('layout_fullscreen.html') },
+            { route: ['', 'index'], name: 'index', moduleId: PLATFORM.moduleName('index'), layoutView: PLATFORM.moduleName('layout_landing.html') },
         ]);
 
         this.router = router;
---
+++ source/web_legacy/webpack.parts.js
@@ -0,0 +1,78 @@
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+let sassBuilder = new ExtractTextPlugin({
+    filename: "[name].[hash:5].bundle.css",
+    allChunks: true
+});
+
+module.exports = {
+    loaders: {
+        htmlLoader: {
+            test: /\.html$/,
+            exclude: /layout/,
+            use: [
+                {
+                    loader: 'html-loader',
+                    options: {
+                        attrs: ["img:src", "link:href"],
+                        interpolate: true,
+                    }
+                }],
+        },
+        sassLoader: {
+            test: /\.scss$/,
+            loader: sassBuilder.extract([
+                "css-loader",
+                "sass-loader",
+                {
+                    loader: "postcss-loader",
+                    options: {
+                        plugins: () => ([
+                            require("autoprefixer")()
+                        ])
+                    }
+                }])
+        },
+        svgLoader: {
+            test: /\.svg/,
+            use: {
+                loader: 'svg-inline-loader',
+                options: {
+                    removeTags: true,
+                    removingTags: ['title', 'desc'],
+                    removingTagAttrs: ['id', 'data-name']
+
+                }
+            }
+        },
+        imageLoader: {
+            test: /\.(png|jpe?g|gif)(\?.*)?$/,
+            use: [{
+                loader: 'file-loader',
+                options: {
+                    name: "[name]-[hash:5].[ext]",
+                    outputPath: "images/"
+                }
+            }
+            ]
+        }
+    },
+    plugins: {
+        cleanDistFolder: new CleanWebpackPlugin(["dist/*"]),
+        buildHtmlIndex: new HtmlWebpackPlugin({
+            template: "./app/layout-header.html",
+            filename: "index.html"
+        }),
+        buildHtmlBoundedContext: new HtmlWebpackPlugin({
+            template: "./app/layout-leftmenu.html",
+            filename: "bounded-context/index.html"
+        }),        
+        buildHtmlBuild: new HtmlWebpackPlugin({
+            template: "./app/layout-leftmenu.html",
+            filename: "bounded-context/build/index.html"
+        }),
+        sassBuilder: sassBuilder
+    }
+}
