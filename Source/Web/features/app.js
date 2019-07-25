d8f8d6b9bb0ce74e76b63ab1c0078817f7614fdc
Retna Ramachandran <retna@dolittle.com>
2019-07-25 14:59:48 +0200

    Project is up to speed with Aurelia changes

--- Source/Web/features/app.js
+++ Source/Web/features/app.js
@@ -9,7 +9,8 @@ export class app {
             { route: ['', 'home'], name: 'index', moduleId: PLATFORM.moduleName('home/index'), layoutView: PLATFORM.moduleName('layout_landing.html') },
             { route: 'applications', name: 'applications', moduleId: PLATFORM.moduleName('applications/index'), layoutView: PLATFORM.moduleName('layout_landing.html') },
             { route: 'build', name: 'build', moduleId: PLATFORM.moduleName('build/index') },
-            { route: 'monitor', name: 'monitor', moduleId: PLATFORM.moduleName('monitor/index') }
+            { route: 'monitor', name: 'monitor', moduleId: PLATFORM.moduleName('monitor/index') },
+            { route: 'continuousimprovement', name: 'continuousimprovement', moduleId: PLATFORM.moduleName('continuousimprovement/index') }
         ]);
 
         this.router = router;
--- Source/Web/webpack.config.js
+++ Source/Web/webpack.config.js
@@ -1,2 +1,9 @@
-const config = require('dolittle.javascript.build.aurelia/webpack.config.js');
+const path = require('path');
+require('dotenv').config();
+
+process.env.DOLITTLE_WEBPACK_OUT = path.resolve('./build');
+
+const config = require('@dolittle/build.aurelia/webpack.config.js');
+
+
 module.exports = config;
