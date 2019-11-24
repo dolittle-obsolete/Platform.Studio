b55cbe8aa03abe3e596cce453522cd8e655a0ec2
Sindre Wilting <sindrewilting@gmail.com>
2019-11-24 12:38:47 +0100

    Make it work

--- Source/Tooling/Plugin/index.ts
+++ Source/Tooling/Plugin/index.ts
@@ -2,10 +2,10 @@
 *  Copyright (c) Dolittle. All rights reserved.
 *  Licensed under the MIT License. See LICENSE in the project root for license information.
 *--------------------------------------------------------------------------------------------*/
-import {Plugin} from '@dolittle/tooling.common.plugins';
+import { Plugin } from '@dolittle/tooling.common.plugins';
 import { plugin as edgePlugin } from '@dolittle/tooling.edge-studio';
 import { loginService, contexts } from '@dolittle/tooling.common.login';
-import {CommandGroupsProvider, CommandsProvider, NamespaceProvider, LoginCommand, AuthenticatedCommandTest} from './internal';
+import { CommandGroupsProvider, CommandsProvider, NamespaceProvider, LoginCommand } from './internal';
 
 let edgePluginCommands = edgePlugin.commandsProvider.provide();
 let edgePluginCommandGroups = edgePlugin.commandGroupsProvider.provide();
@@ -14,8 +14,7 @@ let edgePluginNamespaces = edgePlugin.namespaceProvider.provide();
 export let commandGroupsProvider = new CommandGroupsProvider([...edgePluginCommandGroups,
 ]);
 export let commandsProvider = new CommandsProvider([...edgePluginCommands,
-    new LoginCommand(loginService),
-    new AuthenticatedCommandTest(loginService, contexts)
+    new LoginCommand(loginService)
 ]);
 export let namespaceProvider = new NamespaceProvider([...edgePluginNamespaces,
 
